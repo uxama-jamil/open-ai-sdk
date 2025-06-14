b15b1f7e2f5b42e42dabf25f2fd362a6ac1a4839
Muhammad Usama Jamil <usamajamil@pk.folio3.com>
2025-06-14 18:00:05 +0500

    output  guardrails

---
+++ src/testprj/output-guardrails.py
@@ -0,0 +1,86 @@
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel, set_tracing_disabled,function_tool,handoff
+import requests
+import asyncio
+from dataclasses import dataclass
+from agents import RunContextWrapper
+
+#guardrails
+from pydantic import BaseModel
+from agents import (
+    Agent,
+    GuardrailFunctionOutput,
+    InputGuardrailTripwireTriggered,
+    RunContextWrapper,
+    Runner,
+    TResponseInputItem,
+    input_guardrail,
+)
+
+#output guardrails
+from pydantic import BaseModel
+from agents import (
+    Agent,
+    GuardrailFunctionOutput,
+    OutputGuardrailTripwireTriggered,
+    RunContextWrapper,
+    Runner,
+    output_guardrail,
+)
+
+set_tracing_disabled(True)
+# Set up the external language model provider (Google's Gemini, using OpenAI-compatible API)
+externalProvider = AsyncOpenAI(
+    api_key="AIzaSyBc9lTEos9wzNpVcprceC9I1YfuQvNHmBk",  # API key for Gemini
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",  # Gemini-compatible endpoint
+)
+
+# Create a chat completions model using the Gemini model via the OpenAI API interface
+model = OpenAIChatCompletionsModel(
+    model="gemini-1.5-flash",  # Name of the Gemini model to use
+    openai_client=externalProvider  # The provider configured above
+)
+
+
+class MessageOutput(BaseModel): 
+    response: str
+
+class MathOutput(BaseModel): 
+    reasoning: str
+    is_math: bool
+
+guardrail_agent = Agent(
+    name="Guardrail check",
+    instructions="Check if the output includes any math.",
+    model=model,
+    output_type=MathOutput,
+)
+
+@output_guardrail
+async def math_guardrail(  
+    ctx: RunContextWrapper, agent: Agent, output: MessageOutput
+) -> GuardrailFunctionOutput:
+    result = await Runner.run(guardrail_agent, output.response, context=ctx.context)
+
+    return GuardrailFunctionOutput(
+        output_info=result.final_output,
+        tripwire_triggered=result.final_output.is_math,
+    )
+
+agent = Agent( 
+    name="Customer support agent",
+    instructions="You are a customer support agent. You help customers with their questions.",
+    model=model,
+    output_guardrails=[math_guardrail],
+    output_type=MessageOutput,
+)
+
+async def main():
+    # This should trip the guardrail
+    try:
+        await Runner.run(agent, "Hello, what is the weather in karachi?")
+        print("Guardrail didn't trip - this is unexpected")
+
+    except OutputGuardrailTripwireTriggered:
+        print("Math output guardrail tripped")
+
+asyncio.run(main())
