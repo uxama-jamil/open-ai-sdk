e6135e3ce884ad721738d8fbcc346521d0172f75
Muhammad Usama Jamil <usamajamil@pk.folio3.com>
2025-06-22 21:04:47 +0500

    tool use behavior bypass llm verfication

--- src/testprj/toolSetting.py
+++ src/testprj/toolSetting.py
@@ -4,18 +4,18 @@ from agents import Agent, ModelSettings, Runner, AsyncOpenAI, OpenAIChatCompleti
 from agents.agent import StopAtTools
 
 # Set up the external language model provider (Google's Gemini, using OpenAI-compatible API)
-# externalProvider = AsyncOpenAI(
-#     api_key="AIzaSyBc9lTEos9wzNpVcprceC9I1YfuQvNHmBk",  # API key for Gemini
-#     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",  # Gemini-compatible endpoint
-# )
-externalProvider = AsyncOpenAI(  # API key for Gemini
-    api_key="1234567890",
-    base_url="http://localhost:11434/api/chat",  # Gemini-compatible endpoint
+externalProvider = AsyncOpenAI(
+    api_key="AIzaSyBc9lTEos9wzNpVcprceC9I1YfuQvNHmBk",  # API key for Gemini
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",  # Gemini-compatible endpoint
 )
+# externalProvider = AsyncOpenAI(  # API key for Gemini
+#     api_key="1234567890",
+#     base_url="http://localhost:11434/api/chat",  # Gemini-compatible endpoint
+# )
 
 # Create a chat completions model using the Gemini model via the OpenAI API interface
 model = OpenAIChatCompletionsModel(
-    model="llama3.2:latest",  # Name of the Gemini model to use
+    model="gemini-1.5-flash",  # Name of the Gemini model to use
     openai_client=externalProvider  # The provider configured above
 )
 
@@ -40,7 +40,7 @@ def add(a: float, b: float) -> str:
 def subtract(a: float, b: float) -> str:
     tool_call_count['subtract'] += 1
     print("Tool subtract called")
-    return f"{a} - {b} = {a - b}"
+    return f"{a} - {b} = {a - b+1000}"
 
 @function_tool
 def multiply(a: float, b: float) -> str:
@@ -111,5 +111,32 @@ def  runAgentAsync ():
     # Print the agent's final response
     print(response.final_output)
     
-runAgentAsync()
+def  runAgentAsyncWithoutllmVerification ():
+    # Create an agent with a name and some instructions
+    print(f"running asyncagent")
+    agent = Agent(
+        name="Assistant",
+        instructions="You are a helpful assistant",
+        model=model,  # Uses the Gemini model set above
+        tools=[subtract,add,multiply,divide], # This is the tool that will be used to get the temperature of the city
+        tool_use_behavior='stop_on_first_tool', #llm will not verify the tool call result and will stop at the first tool call
+        
+    )
+
+    # Run the agent synchronously with a given input
+    response = asyncio.run(Runner.run(
+        max_turns=2,
+        starting_agent=agent,
+        input="what is 2-1?"  # Input message to the assistant
+    ))
+    print('tool_call_count',tool_call_count)
+    # running asyncagent
+    # Tool subtract called
+    # tool_call_count {'add': 0, 'subtract': 1, 'multiply': 0, 'divide': 0}
+    # 2.0 - 1.0 = 1001.0  #this is the result of the first tool call
+
+    # Print the agent's final response
+    print(response.final_output)
+    
+runAgentAsyncWithoutllmVerification()
     
